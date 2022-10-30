96d440a9e71019ef7e40efc4b55555bb90cf320f
b0rr4ch4 <flaviowenzel1996@gmail.com>
2022-10-29 23:30:34 -0300

    Lição 1 x86

--- src/01/x86/01.asm
+++ src/01/x86/01.asm
@@ -1,15 +1,15 @@
 ; Olá Mundo!! (com erro)
 ; $ nasm -f elf 01.asm && ld -m elf_i386 01.o -o ./01 && rm 01.o
 
-SECTION .data
-msg	db	'Olá Mundo!!', 0Ah	; 'Olá Mundo!!\n'
-
-SECTION .text
 global	_start
 
-_start:
-	mov	ebx, 1		;arg0
-	mov ecx, msg	;arg1 move o endereço de memoria da msg para dentro do ecx
-	mov	edx, 13		;arg2 número de bytes que serão escritos. Caracter a caracter.
-	mov	eax, 4		;SYS_WRITE (kernel opcode 4)
-	int	80h			;realiza a invocação do eax (kernel opcode 4)
+SECTION .data
+	msg	db	'Olá Mundo!!', 0Ah	; 'Olá Mundo!!\n'
+
+SECTION .text
+	_start:
+		mov	ebx, 1		; ebx (arg0) = file descriptor
+		mov ecx, msg	; ecx (arg1) = endereço da string
+		mov	edx, 13		; edx (arg2) = tamanho da string
+		mov	eax, 4		; eax = código 4 para syscall write
+		int	80h			; invoca o eax (write)
