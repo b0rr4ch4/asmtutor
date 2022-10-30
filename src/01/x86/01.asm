c061bf08a56095b868c33aa1e1651d0bcbe936a6
b0rr4ch4 <flaviowenzel1996@gmail.com>
2022-10-29 23:10:57 -0300

    Lição 1 x86

--- src/01/x86/01.asm
+++ src/01/x86/01.asm
@@ -2,7 +2,7 @@
 ; $ nasm -f elf 01.asm && ld -m elf_i386 01.o -o ./01 && rm 01.o
 
 SECTION .data
-msg	db	'Olá Mundo!!', 0Ah	; 'HOlá Mundo!!\n'
+msg	db	'Olá Mundo!!', 0Ah	; 'Olá Mundo!!\n'
 
 SECTION .text
 global	_start
@@ -10,6 +10,6 @@ global	_start
 _start:
 	mov	ebx, 1		;arg0
 	mov ecx, msg	;arg1 move o endereço de memoria da msg para dentro do ecx
-	mov	edx, 12		;arg2 número de bytes que serão escritos. Caracter a caracter.
+	mov	edx, 13		;arg2 número de bytes que serão escritos. Caracter a caracter.
 	mov	eax, 4		;SYS_WRITE (kernel opcode 4)
 	int	80h			;realiza a invocação do eax (kernel opcode 4)
